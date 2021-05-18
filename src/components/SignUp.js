c854403fb1f634a3802837523a5ad512eae1e81a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-18 22:18:57 +0500

    add SignUp component

---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SignUp = () => {
+  const s = () => 'SignUp';
+
+  return (
+    <div>
+      <h1>{s}</h1>
+      <h1>Helllllo</h1>
+    </div>
+  );
+};
+
+export default SignUp;
