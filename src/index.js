b8622f09cf044e77485ce46d9e28a18dff89453a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-18 22:20:37 +0500

    update index js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery'; //eslint-disable-line
+import Popper from 'popper.js'; //eslint-disable-line
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './styles/App.scss';
+import App from './components/App';
 
-ReactDOM.render(
+const RootApp = () => (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
+ReactDOM.render(<RootApp />, document.getElementById('root'));
