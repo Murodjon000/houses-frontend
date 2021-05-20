3edc23e5503a447564b4e092b345855ae631bd1a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-20 22:20:11 +0500

    update Header js component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from '@reach/router';
 
-const Header = () => (
-  <div className="d-flex justify-content-between py-3 align-items-center">
-    <div>
-      <h1 className="fs-2 text-white fw-bolder">HouseNet</h1>
-    </div>
-    <div className="d-flex align-items-center">
-      <h4 className="px-4  text-white fw-bolder text-uppercase">Sign in</h4>
-      <Link to="/signup" className="header-btn  text-uppercase">
-        Sign up
-      </Link>
-    </div>
-  </div>
-);
+const Header = () => {
+  const [small, setSmall] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener(
+        'scroll',
+        () => setSmall(window.pageYOffset > 100) // eslint-disable-line
+      );
+    }
+  }, []);
+
+  return (
+    <header
+      className={`d-flex justify-content-between py-3 align-items-center ${
+        small ? 'sticy__navbar' : ''
+      }`}
+    >
+      <div>
+        <Link
+          to="/"
+          className={`text-font-md text-decoration-none text-white font-weight-bolder ${
+            small ? 'text-primary' : ''
+          } `}
+        >
+          HouseNet
+        </Link>
+      </div>
+      <div className="d-flex align-items-center">
+        <Link
+          to="/login"
+          className={`px-4  text-white text-decoration-none font-weight-bolder text-uppercase ${
+            small ? 'text-primary' : ''
+          }`}
+        >
+          Sign in
+        </Link>
+        <Link to="/signup" className="header-btn  text-uppercase">
+          Sign up
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
