327546ccb005eff2915260519d0ff53f47d59383
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-20 22:20:38 +0500

    update InfoThird js component

--- src/components/InfoThird.js
+++ src/components/InfoThird.js
@@ -3,7 +3,9 @@ import { Link } from '@reach/router';
 
 const InfoThird = () => (
   <div className="bottom__wrapper d-flex flex-column">
-    <h1 className="text-center text-white my-5">Be everywhere at home</h1>
+    <h1 className="text-center text-white font-weight-bold my-5">
+      Be everywhere at home
+    </h1>
     <div className="mx-auto">
       <Link to="/signup" className="main-btn my-5">
         START FREE
