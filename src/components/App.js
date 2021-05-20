8a15868691cbc6bef26eb779d3c7cdbf0e80699d
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-20 22:19:50 +0500

    update App js component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Router } from '@reach/router';
 import Main from './Main';
 import SignUp from './SignUp';
+import Login from './Login';
+import User from './User';
+import Houses from './Houses';
 
 const App = () => (
   <div>
     <Router>
       <Main path="/" />
       <SignUp path="/signup" />
+      <Login path="/login" />
+      <User path="/user/:id" />
+      <Houses path="/houses" />
     </Router>
   </div>
 );
