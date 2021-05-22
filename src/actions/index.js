dcf5496eef8512670c871bee14281e1db01781d8
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-23 00:26:12 +0500

    update index js action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ export const ADD_USER = 'ADD_USER';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAILURE = 'AUTH_FAILURE';
 export const ADD_HOUSES = 'ADD_HOUSES';
+export const ADD_HOUSES_DETAILS = 'ADD_HOUSES_DETAILS';
+export const ADD_USER_FAVOURITES = 'ADD_USER_FAVOURITES';
 
 /* eslint-disable */
 
@@ -22,5 +24,15 @@ export const authFailure = (errors) => ({
 
 export const addHouses = (houses) => ({
   type: ADD_HOUSES,
+  payload: { houses },
+});
+
+export const addHouseDetails = (house) => ({
+  type: ADD_HOUSES_DETAILS,
+  payload: { house },
+});
+
+export const addUserFavourites = (houses) => ({
+  type: ADD_USER_FAVOURITES,
   payload: { houses },
 });
