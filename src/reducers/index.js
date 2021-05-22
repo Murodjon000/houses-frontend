07f1ab6a470d37e28f8c0a14ccaa9eb8ed54cc5b
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-23 00:27:58 +0500

    add favourites to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './user';
 import houses from './houses';
+import favourites from './favourites';
 
 export default combineReducers({
   user,
   houses,
+  favourites,
 });
