b75fe4bfa3c1cf334af5a82b4a160ddc1e2e46cf
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-23 00:27:42 +0500

    update houses reducer

--- src/reducers/houses.js
+++ src/reducers/houses.js
@@ -1,8 +1,12 @@
-import { ADD_HOUSES } from '../actions';
+import { ADD_HOUSES, ADD_HOUSES_DETAILS } from '../actions';
 
 const housesRed = (state = {}, action) => {
   if (action.type === ADD_HOUSES) {
     return { ...state, houses: action.payload.houses };
+  }
+
+  if (action.type === ADD_HOUSES_DETAILS) {
+    return { ...state, house: action.payload.house };
   }
 
   return state;
