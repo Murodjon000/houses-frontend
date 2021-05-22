d0e2b7ffbef2ef95ec44bf7b8dbe9253791bd8f9
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-23 00:24:26 +0500

    update App js router config

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,31 @@ import SignUp from './SignUp';
 import Login from './Login';
 import User from './User';
 import Houses from './Houses';
+import HouseDetail from './HouseDetail';
+import Dashboard from './Dashboard';
 
-const App = () => (
-  <div>
-    <Router>
-      <Main path="/" />
-      <SignUp path="/signup" />
-      <Login path="/login" />
-      <User path="/user/:id" />
-      <Houses path="/houses" />
-    </Router>
-  </div>
-);
+const App = () => {
+  if (!localStorage.getItem('token')) {
+    return (
+      <div>
+        <Router>
+          <Main path="/" />
+          <SignUp path="/signup" />
+          <Login path="/login" />
+        </Router>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Router>
+        <Dashboard path="/" />
+        <User path="/user/:id" />
+        <Houses path="/houses" />
+        <HouseDetail path="/houses/:id" />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
