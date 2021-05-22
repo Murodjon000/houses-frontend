e5ba8192be52acefbca17cadc7672ee982cbb079
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-23 00:27:18 +0500

    update User component

--- src/components/User.js
+++ src/components/User.js
@@ -1,19 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+// import { Link } from '@reach/router';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { getUser } from '../helpers/api_calls';
+import { authSuccess } from '../actions';
+import FavouritesCard from './FavouritesCard';
 
-const User = () => {
-  const [name, setName] = useState('');
+/* eslint-disable */
 
-  useEffect(async () => {
-    const data = await getUser().then((response) => response);
-    setName(data.user.username);
+const User = ({ user, getUserData }) => {
+  useEffect(() => {
+    getUser(getUserData);
   }, []);
 
+  if (!user) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className="">
+      <div className="user__info px-3">
+        <div className="d-flex flex-wrap bg-white rounded p-3 mt-4">
+          {user.favourites.map((house) => (
+            <FavouritesCard
+              name={house.name}
+              price={house.price}
+              image={house.image}
+              location={house.location}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default User;
+User.propTypes = {
+  user: PropTypes.any, //eslint-disable-line
+
+  getUserData: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user.user,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserData: (user) => dispatch(authSuccess(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(User);
