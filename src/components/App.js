bed1cb04f1f1e3cf4a37b0b73de2f285b54c2714
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-24 19:13:58 +0500

    update App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,33 +3,21 @@ import { Router } from '@reach/router';
 import Main from './Main';
 import SignUp from './SignUp';
 import Login from './Login';
-import User from './User';
 import Houses from './Houses';
 import HouseDetail from './HouseDetail';
 import Dashboard from './Dashboard';
 
-const App = () => {
-  if (!localStorage.getItem('token')) {
-    return (
-      <div>
-        <Router>
-          <Main path="/" />
-          <SignUp path="/signup" />
-          <Login path="/login" />
-        </Router>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <Router>
-        <Dashboard path="/" />
-        <User path="/user/:id" />
-        <Houses path="/houses" />
-        <HouseDetail path="/houses/:id" />
-      </Router>
-    </div>
-  );
-};
+const App = () => (
+  <div id="outer-container">
+    <Router>
+      <Main path="/" />
+      <Dashboard path="/dashboard" />
+      <Houses path="/houses" />
+      <HouseDetail path="/houses/:id" />
+      <Login path="/login" />
+      <SignUp path="/signup" />
+    </Router>
+  </div>
+);
 
 export default App;
