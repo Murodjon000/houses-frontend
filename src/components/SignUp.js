697f3613290b66d31d185f71f7c7fc3912576d81
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-26 15:16:53 +0500

    add signup error message

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
+import { Alert } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { authCalls } from '../helpers/api_calls';
-import { createUser } from '../actions';
+import { createUser, signupFailure } from '../actions';
 
-const SignUp = ({ addUser }) => {
+const SignUp = ({ addUser, authError, errors }) => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -17,10 +18,9 @@ const SignUp = ({ addUser }) => {
       username,
       email,
       password,
-      passwordConfirmation,
+      password_confirmation: passwordConfirmation,
     };
-    authCalls('signup', user);
-    addUser(user);
+    authCalls('signup', user, addUser, authError);
   };
 
   const onUsernameChange = (e) => {
@@ -44,59 +44,85 @@ const SignUp = ({ addUser }) => {
   };
 
   return (
-    <div className="signup__wrapper  d-flex flex-column justify-content-center align-items-center">
-      <h1 className="text-font-md mb-2">Sign up</h1>
-      <p className="text-center">
-        Hi there! Sign up and start looking houses from anywhere
-      </p>
-      <div className="signup__wrapper-form">
-        <form className="d-flex flex-column my-3" onSubmit={handleUserSubmit}>
-          <input
-            type="text"
-            placeholder="Enter your username"
-            className="main-input"
-            onChange={onUsernameChange}
-          />
-          <input
-            type="email"
-            className="main-input my-3"
-            placeholder="Enter your email"
-            onChange={onEmailChange}
-          />
-          <input
-            type="password"
-            className="main-input "
-            placeholder="Enter your password"
-            onChange={onPasswordChange}
-          />
-          <input
-            type="password"
-            className="main-input my-3"
-            placeholder="Confirm your password"
-            onChange={onPasswordConfChange}
-          />
-          <input
-            type="submit"
-            className="main-btn primary-shadow"
-            value="Sign up"
-          />
-        </form>
-      </div>
+    <div className="signup__wrapper   d-flex flex-column flex-wrap">
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h1 className=" text-center text-font-md mb-2">Sign up</h1>
+        <p className="text-center mb-2">
+          Hi there! Sign up and start looking houses from anywhere
+        </p>
+        <div className="errors w-100">
+          {errors ? (
+            <div>
+              <Alert key="6" variant="danger">
+                {errors.data.message.map((item) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <li key={Date.now() * Math.random(2)}>{item}</li>
+                ))}
+              </Alert>
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+        <div className="signup__wrapper-form w-100">
+          <form className="d-flex flex-column my-3" onSubmit={handleUserSubmit}>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              className="main-input"
+              onChange={onUsernameChange}
+            />
+            <input
+              type="email"
+              className="main-input my-3"
+              placeholder="Enter your email"
+              onChange={onEmailChange}
+            />
+            <input
+              type="password"
+              className="main-input "
+              placeholder="Enter your password"
+              onChange={onPasswordChange}
+            />
+            <input
+              type="password"
+              className="main-input my-3"
+              placeholder="Confirm your password"
+              onChange={onPasswordConfChange}
+            />
+            <input
+              type="submit"
+              className="main-btn primary-shadow"
+              value="Sign up"
+            />
+          </form>
+        </div>
 
-      <p>Have an account?</p>
-      <Link to="/login" className="text-decoration-none text-hover">
-        Login
-      </Link>
+        <p>Have an account?</p>
+        <Link
+          to="/login"
+          className="text-decoration-none text-center text-hover"
+        >
+          Login
+        </Link>
+      </div>
     </div>
   );
 };
 
 SignUp.propTypes = {
   addUser: PropTypes.func.isRequired,
+  errors: PropTypes.object, // eslint-disable-line
+  authError: PropTypes.func.isRequired,
 };
+
+const mapStateToProps = (state) => ({
+  errors: state.user.signupErrors,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   addUser: (user) => dispatch(createUser(user)),
+  authError: (error) => dispatch(signupFailure(error)),
 });
 
-export default connect(null, mapDispatchToProps)(SignUp);
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
