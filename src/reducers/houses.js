fdac8a7bc47032ad0acfd75fff21d2523506cb93
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-26 15:19:29 +0500

    add houses errors reducers

--- src/reducers/houses.js
+++ src/reducers/houses.js
@@ -1,12 +1,25 @@
-import { ADD_HOUSES, ADD_HOUSES_DETAILS } from '../actions';
+import {
+  ADD_HOUSES,
+  ADD_HOUSES_DETAILS,
+  GET_HOUSE_DETAIL_ERRORS,
+  GET_HOUSE_ERRORS,
+} from '../actions';
 
 const housesRed = (state = {}, action) => {
   if (action.type === ADD_HOUSES) {
     return { ...state, houses: action.payload.houses };
   }
 
+  if (action.type === GET_HOUSE_ERRORS) {
+    return { ...state, errors: action.payload.errors };
+  }
+
   if (action.type === ADD_HOUSES_DETAILS) {
     return { ...state, house: action.payload.house };
+  }
+
+  if (action.type === GET_HOUSE_DETAIL_ERRORS) {
+    return { ...state, houseError: action.payload.errors };
   }
 
   return state;
