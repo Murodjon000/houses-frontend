63dc3134f66112ad99c163ca53b1bfd2a6f3b4b9
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-26 15:19:45 +0500

    add houses and users errors actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
 export const ADD_USER = 'ADD_USER';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAILURE = 'AUTH_FAILURE';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
 export const ADD_HOUSES = 'ADD_HOUSES';
+export const GET_HOUSE_ERRORS = 'GET_HOUSE_ERRORS';
+export const GET_HOUSE_DETAIL_ERRORS = 'GET_HOUSE_DETAIL_ERRORS';
 export const GET_USER = 'GET_USER';
+export const GET_USER_FAILURE = 'GET_USER_FAILURE';
 export const ADD_HOUSES_DETAILS = 'ADD_HOUSES_DETAILS';
 
 /* eslint-disable */
@@ -22,8 +26,18 @@ export const getCurrentUser = (user) => ({
   payload: { user },
 });
 
+export const getUserFailure = (errors) => ({
+  type: GET_USER_FAILURE,
+  payload: { errors },
+});
+
 export const authFailure = (errors) => ({
   type: AUTH_FAILURE,
+  payload: { errors },
+});
+
+export const signupFailure = (errors) => ({
+  type: SIGNUP_FAILURE,
   payload: { errors },
 });
 
@@ -32,7 +46,17 @@ export const addHouses = (houses) => ({
   payload: { houses },
 });
 
+export const getHousesErrors = (errors) => ({
+  type: GET_HOUSE_ERRORS,
+  payload: { errors },
+});
+
 export const addHouseDetails = (house) => ({
   type: ADD_HOUSES_DETAILS,
   payload: { house },
+});
+
+export const getHousesDetailsErrors = (errors) => ({
+  type: GET_HOUSE_DETAIL_ERRORS,
+  payload: { errors },
 });
