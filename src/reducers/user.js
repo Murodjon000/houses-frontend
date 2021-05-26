192224f96a4ec59939f4dff495d7581c01f1e0fa
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-26 15:19:19 +0500

    add user errors reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,4 +1,12 @@
-import { ADD_USER, AUTH_SUCCESS, GET_USER } from '../actions';
+// eslint-disable-next-line
+import {
+  ADD_USER,
+  AUTH_FAILURE,
+  AUTH_SUCCESS,
+  GET_USER,
+  GET_USER_FAILURE,
+  SIGNUP_FAILURE,
+} from '../actions';
 
 const user = (state = {}, action) => {
   if (action.type === ADD_USER) {
@@ -8,10 +16,21 @@ const user = (state = {}, action) => {
     return { ...state, user: action.payload.user };
   }
 
+  if (action.type === AUTH_FAILURE) {
+    return { ...state, loginErrors: action.payload.errors };
+  }
+
+  if (action.type === SIGNUP_FAILURE) {
+    return { ...state, signupErrors: action.payload.errors };
+  }
+
   if (action.type === GET_USER) {
     return { ...state, userData: action.payload.user };
   }
 
+  if (action.type === GET_USER_FAILURE) {
+    return { ...state, userError: action.payload.errors };
+  }
   return state;
 };
 
