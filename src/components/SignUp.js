5f94bdf95da496c65066ec44940513e430596a8f
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-04 16:15:15 +0500

    update Signup component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,44 +3,33 @@ import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
 import { Alert } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { authCalls } from '../helpers/api_calls';
+// import { authCalls } from '../helpers/api_calls';
 import { createUser, signupFailure } from '../actions';
+import { createUserSign } from '../helpers/upload_calls';
+import Flash from './Flash';
 
 const SignUp = ({ addUser, authError, errors }) => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfiramtion] = useState('');
+  const [file, setFile] = useState('');
 
   const handleUserSubmit = (e) => {
     e.preventDefault();
-    const user = {
-      username,
-      email,
-      password,
-      password_confirmation: passwordConfirmation,
-    };
-    authCalls('signup', user, addUser, authError);
-  };
 
-  const onUsernameChange = (e) => {
-    const { value } = e.target;
-    setUsername(value);
-  };
-
-  const onEmailChange = (e) => {
-    const { value } = e.target;
-    setEmail(value);
-  };
-
-  const onPasswordChange = (e) => {
-    const { value } = e.target;
-    setPassword(value);
-  };
-
-  const onPasswordConfChange = (e) => {
-    const { value } = e.target;
-    setPasswordConfiramtion(value);
+    if (file !== '') {
+      const user = {
+        username,
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+        image: file,
+      };
+      createUserSign(user, addUser, authError);
+    } else {
+      window.flash('Avatar shoud be selected!', 'warning');
+    }
   };
 
   return (
@@ -50,6 +39,7 @@ const SignUp = ({ addUser, authError, errors }) => {
         <p className="text-center mb-2">
           Hi there! Sign up and start looking houses from anywhere
         </p>
+        <Flash />
         <div className="errors w-100">
           {errors ? (
             <div>
@@ -70,26 +60,35 @@ const SignUp = ({ addUser, authError, errors }) => {
               type="text"
               placeholder="Enter your username"
               className="main-input"
-              onChange={onUsernameChange}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <input
               type="email"
               className="main-input my-3"
               placeholder="Enter your email"
-              onChange={onEmailChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="password"
               className="main-input "
               placeholder="Enter your password"
-              onChange={onPasswordChange}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <input
               type="password"
               className="main-input my-3"
               placeholder="Confirm your password"
-              onChange={onPasswordConfChange}
+              onChange={(e) => setPasswordConfiramtion(e.target.value)}
             />
+            <label htmlFor="avatar">
+              Upload Avatar
+              <input
+                type="file"
+                className="w-100 my-3"
+                onChange={(e) => setFile(e.target.files[0])}
+                id="avatar"
+              />
+            </label>
             <input
               type="submit"
               className="main-btn primary-shadow"
